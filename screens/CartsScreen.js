39ef064da2369d9dcbaf1e00afc7440a68e9411a
Julia Macias <julyemacias@gmail.com>
2024-11-20 12:30:09 -0500

    ULTIMO CAMBIO

--- screens/CartsScreen.js
+++ screens/CartsScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, FlatList, Button, Modal, ScrollView, Image } from 'react-native';
+import { View, TextInput, FlatList, Button, Modal, ScrollView, Image, Text } from 'react-native';
 import axios from 'axios';
 import initializeDB, { cartsCollectionName } from '../database/rxdb';
 import uuid from 'react-native-uuid';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
+import { stringify,parse } from 'flatted';
 
 const CartsScreen = () => {
   const [carts, setCarts] = useState([]);
@@ -12,7 +13,7 @@ const CartsScreen = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [db, setDb] = useState(null);
   const [isInCollection, setIsInCollection] = useState(false);
-  const [editedProductQuantities, setEditedProductQuantities] = useState({});
+ 
 
   useEffect(() => {
     const initDB = async () => {
@@ -20,7 +21,7 @@ const CartsScreen = () => {
   
       try {
         const DB = await initializeDB();
-        console.log('Base de datos inicializada:', DB);
+      
         setDb(DB);
       } catch (error) {
         console.error('Error al inicializar la base de datos:', error);
@@ -31,7 +32,7 @@ const CartsScreen = () => {
     const fetchCartsData = async () => {
       try {
         const response = await axios.get('https://dummyjson.com/carts');
-        console.log("Respuesta de la API:", response.data);
+      
         if (Array.isArray(response.data)) {
           setCarts(response.data);
         } else if (response.data.carts) {
@@ -48,6 +49,7 @@ const CartsScreen = () => {
 
     fetchCartsData();
     initDB();
+      
   }, []);
 
   const handleSelectCart = async (cart) => {
@@ -55,7 +57,6 @@ const CartsScreen = () => {
     const cartsCollection = db[cartsCollectionName];
     if (cartsCollection) {
       await cartsCollection.insert(cart);
-      console.log('Guardado');
       showMessage({
         message: 'Carrito guardado con éxito!',
         type: 'success',
@@ -65,13 +66,20 @@ const CartsScreen = () => {
   };
 
   const handleOpenModal = async (cart) => {
-    setSelectedCart(cart);
+   
     setEditedProductQuantities({}); // Resetear las cantidades editadas cuando se abre el modal
 
     if (db) {
       const cartsCollection = db[cartsCollectionName];
-      const existingCart = await cartsCollection.findOne({ selector: { uuid: cart.uuid } }).exec();
+      const existingCart = await cartsCollection.findOne({ selector: { userId: cart.userId } }).exec();
       setIsInCollection(!!existingCart);
+      if(existingCart){
+        const recontruido = parse(stringify(existingCart));
+         console.log('ver ' +recontruido );
+        setSelectedCart(recontruido);
+      }else{
+         setSelectedCart(cart);
+      }
     }
 
     setIsModalOpen(true);
@@ -81,18 +89,48 @@ const CartsScreen = () => {
     setIsModalOpen(false);
   };
 
-  const handleEditCart = () => {
-    if (db) {
-      const cartsCollection = db[cartsCollectionName];
-      cartsCollection.upsert(selectedCart);
+  const handleEditCart = async () => {
+    try {
+      console.log('Carrito a guardar:', selectedCart);
+      // Elimina propiedades cíclicas antes de guardar
+      const sanitizedCart = parse(stringify(selectedCart)); // Esto elimina las referencias cíclicas
+      console.log('Carrito antes de editar:', sanitizedCart);
+  
+      if (db) {
+        const cartsCollection = db[cartsCollectionName];
+        await cartsCollection.upsert(sanitizedCart);
+  
+        const updatedCart = await cartsCollection.findOne({selector:{userId: selectedCart.userId}  });
+  
+        if (updatedCart) {
+          console.log('Carrito actualizado:', updatedCart);
+          showMessage({
+            message: 'Carrito editado con éxito!',
+            type: 'success',
+            backgroundColor: 'blue',
+          });
+        } else {
+          console.log('El carrito no se actualizó correctamente');
+          showMessage({
+            message: 'Hubo un problema al editar el carrito',
+            type: 'error',
+            backgroundColor: 'red',
+          });
+        }
+      }
+    } catch (error) {
+      console.error('Error al editar el carrito:', error);
       showMessage({
-        message: 'Carrito editado con éxito!',
-        type: 'success',
-        backgroundColor: 'blue',
+        message: 'Hubo un error al editar el carrito',
+        type: 'error',
+        backgroundColor: 'red',
       });
     }
+  
     handleCloseModal();
   };
+  
+  
 
   const handleDeleteCart = async () => {
     if (db) {
@@ -108,64 +146,64 @@ const CartsScreen = () => {
   };
 
   const handleQuantityChange = (productId, operation) => {
-    // Cambiar la cantidad de acuerdo con la operación (incrementar o decrementar)
-    setEditedProductQuantities(prevState => {
-      const updatedQuantities = { ...prevState };
-      const currentQuantity = updatedQuantities[productId] || selectedCart.products.find(p => p.id === productId)?.quantity || 0;
-      const newQuantity = operation === 'increment' ? currentQuantity + 1 : Math.max(0, currentQuantity - 1);
+    // Asegúrate de que selectedCart y selectedCart.products estén definidos
+    if (!selectedCart?.products) {
+      console.error('selectedCart o selectedCart.products no están definidos');
+      return;
+    }
+    console.log(selectedCart)
 
-      updatedQuantities[productId] = newQuantity;
-
-      // Actualizar el carrito con la nueva cantidad
-      const updatedProducts = selectedCart.products.map(product => {
-        if (product.id === productId) {
-          return { ...product, quantity: newQuantity, total: newQuantity * product.price }; // Recalcular el total
-        }
-        return product;
-      });
-
-      setSelectedCart(prevCart => ({
-        ...prevCart,
-        products: updatedProducts,
-      }));
-
-      return updatedQuantities;
-    });
+  // Crear una nueva versión del carrito con el producto modificado
+  const updatedCart = {
+    ...selectedCart, // Copia el carrito existente
+    products: selectedCart.products.map(pr => {
+      if (pr.id === productId) {
+        // Modificar solo el producto que coincide con el ID
+        const updatedProduct = {
+          ...pr, // Copia el producto
+          quantity: operation === 'increment' ? pr.quantity + 1 : pr.quantity - 1,
+          total: pr.price * (operation === 'increment' ? pr.quantity + 1 : pr.quantity - 1),
+        };
+        return updatedProduct; // Retorna el producto actualizado
+      }
+      return pr; // Retorna los otros productos sin cambios
+    }),
+    total: selectedCart.products.reduce((acc, pr) => acc + pr.total, 0),
+    totalQuantity: selectedCart.products.reduce((acc, pr) => acc + pr.quantity, 0),
   };
 
+  // Actualizar el estado con el carrito modificado
+  setSelectedCart(updatedCart);
+
+  };
+  
   const filteredCarts = filters ? carts.filter(cart => cart.userId == filters) : [...carts];
 
   const renderCartItem = ({ item }) => (
     <View style={{ marginBottom: 10, borderBottomWidth: 1, paddingBottom: 10 }}>
-      <TextInput>Usuario: {item.userId.toString()}</TextInput>
-      <TextInput>Total: ${item.total}</TextInput>
+      <TextInput style={styles.input}>Usuario: {item.userId.toString()}</TextInput>
+      <TextInput style={styles.input}>Total: ${item.total}</TextInput>
       <Button title="Guardar" onPress={() => handleSelectCart(item)} />
       <Button title="Ver Detalles" onPress={() => handleOpenModal(item)} />
     </View>
   );
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 10 }}>
+    <ScrollView contentContainerStyle={{ paddingLeft: 10 }}>
       <FlashMessage />
       {/* Filtros */}
-      <View style={{ marginBottom: 5 }}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={{
-            height: 40,
-            borderColor: '#ccc',
-            borderWidth: 1,
-            paddingLeft: 10,
-            borderRadius: 5,
-          }}
+          style={styles.input}
           value={filters}
-          onChangeTextInput={setFilters}
+          onChangeText={setFilters}
           placeholder="Filtrar por userId"
         />
       </View>
 
       {/* Lista de carritos */}
       <View style={{ marginBottom: 2 }}>
-        <TextInput style={{ fontSize: 18, fontWeight: 'bold' }}>Carritos</TextInput>
+        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Carritos</Text>
         <FlatList
           data={filteredCarts}
           renderItem={renderCartItem}
@@ -180,36 +218,45 @@ const CartsScreen = () => {
           onRequestClose={handleCloseModal}
           animationType="slide"
         >
-          <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-            <TextInput style={{ fontSize: 24, fontWeight: 'bold' }}>Detalle del Carrito</TextInput>
-            <TextInput>ID Usuario: {selectedCart.userId.toString()}</TextInput>
-            <TextInput>Total: ${selectedCart.total}</TextInput>
-            <TextInput>Cantidad de Productos: {selectedCart.totalProducts}</TextInput>
-            <TextInput>Unidades por Productos: {selectedCart.totalQuantity}</TextInput>
+          <View style={styles.view}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Detalle del Carrito</Text>
+            <TextInput style={styles.input}>ID Usuario: {selectedCart.userId?.toString()}</TextInput>
+            <TextInput style={styles.input}>Total: ${selectedCart.total?.toString()}</TextInput>
+            <TextInput style={styles.input}>Cantidad de Productos: {selectedCart.totalProducts?.toString()}</TextInput>
+            <TextInput style={styles.input}>Unidades por Productos: {selectedCart.totalQuantity?.toString()}</TextInput>
             <TextInput style={{ fontSize: 18, fontWeight: 'bold' }}>Productos:</TextInput>
             <FlatList
               data={selectedCart.products}
               keyExtractor={product => product.id.toString()}
               renderItem={({ item }) => (
                 <View style={{ marginBottom: 2 }}>
-                  <TextInput>Producto: {item.title}</TextInput>
-                  <TextInput>Precio: ${item.price}</TextInput>
-                  <TextInput>Total: ${item.total}</TextInput>
-                 
+                  <Text style={styles.input}>Producto: {item.title}</Text>
+                  <Text style={styles.input}>Precio: ${item.price}</Text>
+                  <Text style={styles.input}>Total: ${item.total}</Text>
+
                   {/* Botones de incremento y decremento */}
                   <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                 
+                  {isInCollection && (
+              <>
+                    
                     <Button
                       title="-"
                       onPress={() => handleQuantityChange(item.id, 'decrement')}
                     />
-                    <TextInput style={{ marginHorizontal: 10 }}>
-                    Cantidad: {editedProductQuantities[item.id] || item.quantity}
-                    </TextInput>
+                    <Text style={styles.input}>
+                      Cantidad: {item.quantity}
+                    </Text>
                     <Button
                       title="+"
                       onPress={() => handleQuantityChange(item.id, 'increment')}
                     />
+                     </>
+                  )}
+                  {!isInCollection && (
+                     <TextInput style={styles.input}>
+                      Cantidad: {item.quantity}
+                    </TextInput>
+                  )}
                   </View>
 
                   {item.thumbnail && <Image source={{ uri: item.thumbnail }} style={{ width: 100, height: 100 }} />}
@@ -228,6 +275,22 @@ const CartsScreen = () => {
       )}
     </ScrollView>
   );
+};
+
+const styles = {
+  input: {
+    height: 40,
+
+   // borderColor: '#ccc',
+  // borderWidth: 1,
+    paddingLeft: 10,
+    borderRadius: 5,
+    marginBottom: 10, // Reducido el margen
+  },
+  inputContainer: {
+    marginBottom: 20, // Un poco más de espacio entre los filtros
+  },
+  view:{ flex: 1, justifyContent: 'center', padding: 10 }
 };
 
 export default CartsScreen;
